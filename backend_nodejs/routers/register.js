dc927ff6c72b3ed620eeddbabea0276a6d6f061f
Mikhail Efimenko <ma.efimenko14@gmail.com>
2024-09-11 13:57:28 +0300

    done V2

--- backend_nodejs/routers/register.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const register = require("../controllers/register.js");
-
-const router = Router();
-
-router.post("/auth/register", register.registration);
-router.post("/auth/login", register.login);
-
-module.exports = router;
